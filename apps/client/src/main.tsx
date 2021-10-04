2eaa806f8d9a5863da36276597b9c099fdca6fb7
hritikchokker <hchokker7@gmail.com>
2021-10-04 19:33:02 +0530

    redux added

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
 import App from './app/containers/App';
-
+import { store } from './app/store/store';
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
   document.getElementById('root')
 );
